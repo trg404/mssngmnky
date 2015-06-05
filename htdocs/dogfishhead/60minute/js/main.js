6a65418d39b3365ff11241b17dc7f91c5b37366f
Trog404 <trg404@gmail.com>
2015-06-05 13:38:11 -0500

    success on dogfish head background
    Found and implemented backstretch to figure out background of DFH 60
    min page.

--- htdocs/dogfishhead/60minute/js/main.js
+++ htdocs/dogfishhead/60minute/js/main.js
@@ -1,21 +1,12 @@
-$(function () {
-    var body = $('body');
-    var backgrounds = [
-      'url(img/1.jpg)', 
-      'url(img/2.jpg)',
-      'url(img/3.jpg',
-      'url(img/4.jpg'];
-    var current = 0;
-
-    function nextBackground() {
-        body.css(
-            'background',
-        backgrounds[current = ++current % backgrounds.length]);
-
-        setTimeout(nextBackground, 500);
-    }
-    setTimeout(nextBackground, 500);
-    body.css('background', backgrounds[0]);
-});
-
-
+$.backstretch([
+      "img/1.jpg"
+    , "img/2.jpg"
+    , "img/3.jpg"
+    , "img/4.jpg"
+    , "img/5.jpg"
+    , "img/6.jpg"
+    , "img/7.jpg"
+    , "img/8.jpg"
+    , "img/9.jpg"
+    , "img/10.jpg"
+  ], {duration: 6000, fade: 800});

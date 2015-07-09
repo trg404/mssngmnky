62bb36511d7cd0bcd8cabdcb15f52f319df08ca7
trg404 <trg404@gmail.com>
2015-07-08 21:19:02 -0500

    re arranged the desktop css.

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,12 +1,4 @@
- $('.your-class').slick({
-  dots:true, 
-  autoplay: true,
-  autoplaySpeed: 2000,
-  });
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
+
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
 //# sourceMappingURL=main.js.map
